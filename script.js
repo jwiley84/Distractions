b46f1d697e7f82010f57dd20e646261399d0e871
Jessica Wailes <jess.m.wailes@gmail.com>
2022-08-01 18:30:30 -0700

    walking cat

--- script.js
+++ script.js
@@ -6,14 +6,29 @@ const CANVAS_HEIGHT = canvas.height = 600;
 
 const playerImg = new Image();
 playerImg.src = 'catwalkbluespritesheet.png'
-let test = 0;
+// let test = 0;
+const spriteWidth = 544;
+const spriteHeight = 476;
+let frameX = 2;
+let frameY = 0;
+let gameFrame = 0;
+const staggerFrames = 5;
 
-function animationLoop() {
+function animateWalk() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.fillRect(test, 50, 100, 100);
-    test++;
+    //ctx.fillRect(0, 50, 100, 100);
+    //ctx.drawImage(image, sourcex, sourcey, sourcew, sourceh, destx, desty, destw, desth)
+    ctx.drawImage(playerImg, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+    if (gameFrame % staggerFrames == 0) {
+        if (frameX < 9) frameX++;
+        else frameX = 0;
+    }
 
-    requestAnimationFrame(animationLoop);
+
+    
+    gameFrame++;
+    requestAnimationFrame(animateWalk);
 };
 
-animationLoop();
+animateWalk();
+
